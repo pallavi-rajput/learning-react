e28b84e7500df1a02d78e49ab4442acad44013ee
Pallavi Rajput <pallavirajput93@gmail.com>
2020-02-11 22:08:42 +0530

    learnt passing props and rendering components list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./App.css";
-import SignUp from "./signUp";
-import SignIn from "./signIn";
+// import SignUp from "./signUp";
+// import SignIn from "./signIn";
+import Ninjas from "./Ninjas";
 
 class App extends React.Component {
+  state = {
+    ninjas: [
+      { name: "ryu", age: 27, belt: "brown", id: 1 },
+      { name: "coshi", age: 27, belt: "white", id: 2 },
+      { name: "yoshi", age: 0, belt: "extra-white", id: 3 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <SignUp />
-        <SignIn />
+        {/* <SignUp />
+        <SignIn /> */}
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
---
+++ src/Ninjas.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Ninjas extends Component {
+  render() {
+    const { ninjas } = this.props;
+
+    const ninjaList = ninjas.map(ninja => (
+      <div className="ninjas" key={ninja.id}>
+        <div>name: {ninja.name}</div>
+        <div>age: {ninja.age}</div>
+        <div>color: {ninja.belt}</div>
+      </div>
+    ));
+    return <div>{ninjaList}</div>;
+  }
+}
+
+export default Ninjas;
