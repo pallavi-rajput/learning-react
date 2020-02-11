48584affc561b35c7380a2135b09e2b2523f3169
Pallavi Rajput <pallavirajput93@gmail.com>
2020-02-11 22:22:52 +0530

    learnt passing props and rendering components list

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Ninjas extends Component {
   render() {
     const { ninjas } = this.props;
-
+// rendering components as list
     const ninjaList = ninjas.map(ninja => (
       <div className="ninjas" key={ninja.id}>
         <div>name: {ninja.name}</div>
